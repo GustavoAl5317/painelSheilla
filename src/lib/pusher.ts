4a17ea2e9e476910435000c451d84689c5c24cba
Gustavo Alves Santana <gsantana@interatell.com.br>
2026-04-28 19:20:05 -0300

    versao6

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -1,4 +1,7 @@
-import PusherServer from "pusher";
+import Pusher from "pusher";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const PusherServer = (Pusher as any).default ?? Pusher;
 
 export const pusherServer = new PusherServer({
   appId: process.env.PUSHER_APP_ID!,
@@ -6,4 +9,4 @@ export const pusherServer = new PusherServer({
   secret: process.env.PUSHER_SECRET!,
   cluster: process.env.PUSHER_CLUSTER!,
   useTLS: true,
-});
+}) as Pusher;
